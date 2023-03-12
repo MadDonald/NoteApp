672c750fbb142841d07b1f51e102acbcbb5a3c6e
Jacob MacDonald <100619060+MadDonald@users.noreply.github.com>
2023-03-12 19:55:05 -0400

    Fixed the index button, added server information

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Middleware for parsing incoming request data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// HTML Routes
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+});
+
+// API Routes
+app.get('/api/notes', (req, res) => {
+  const notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+  res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+  const { title, text } = req.body;
+  const notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+  const id = notes.length > 0 ? notes[notes.length - 1].id + 1 : 1;
+  const newNote = { id, title, text };
+  notes.push(newNote);
+  fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+  res.json(newNote);
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  const notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+  const updatedNotes = notes.filter((note) => note.id !== parseInt(req.params.id));
+  fs.writeFileSync('./db/db.json', JSON.stringify(updatedNotes));
+  res.json({ success: true });
+});
+
+// Server Setup
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
